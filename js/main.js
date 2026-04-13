dc74754faccd66fc1c8349d7c62e6fd3496aa2f7
LuckyMonkeyB <luckymonkeyb@gmail.com>
2026-04-13 11:31:27 +0200

    feat: initialize game variables and DOM references

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let pvUser = 100;
+let pvPC = 100;
+
+const attack = document.querySelector('.btn-attack');
+
+const userImg = document.querySelector('.user');
+const pcImg = document.querySelector('.pc');
+
+const jornaling = document.querySelector('.jornal-log');
+
+
